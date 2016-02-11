4f9927f44bd959d0b98b0a5df6dd87b9df2877f8
Mykolai_Lytvyn <mycdpepam@gmail.com>
2016-02-11 10:18:42 +0200

    Connection Pool

--- Junior/ConnectionPull/src/com/company/Main.java
+++ Junior/ConnectionPull/src/com/company/Main.java
@@ -1,16 +1,14 @@
 package com.company;
 
 import com.company.ConnectionFactoryClasses.ConnectionFactoryFactory;
-import com.company.UserDaoClasses.UserDAOClass;
-import com.company.interfaces.ConnectionFactory;
+import com.company.UserDaoClasses.UserDAOCJdbc;
 import com.company.interfaces.UserDAO;
 
 public class Main {
 
     public static void main(String[] args) {
-        ConnectionFactoryFactory.setType(ConnectionFactoryFactory.FactoryType.RAW);
-        UserDAO user = new UserDAOClass();
-
-
+        ConnectionFactoryFactory.setType(ConnectionFactoryFactory.FactoryType.JDBC);
+        UserDAO user = new UserDAOCJdbc();
+        System.out.println(user.selectAll());
     }
 }
---
+++ Junior/ConnectionPull/src/com/company/UserDaoClasses/UserDaoDbcp.java
@@ -0,0 +1,16 @@
+package com.company.UserDaoClasses;
+
+import com.company.UserClasses.User;
+import com.company.interfaces.UserDAO;
+
+import java.util.List;
+
+/**
+ * Created by Mykolai_Lytvyn on 10.02.2016.
+ */
+public class UserDaoDbcp implements UserDAO {
+    @Override
+    public List<User> selectAll() {
+        return null;
+    }
+}
