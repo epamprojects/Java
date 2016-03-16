f902b029360783c65faf2d82be95b945120fba98
Mykolai_Lytvyn <mycdpepam@gmail.com>
2016-03-16 10:28:36 +0200

    servlet

--- Junior/servlet/src/main/java/controllers/QuizController.java
+++ Junior/servlet/src/main/java/controllers/QuizController.java
@@ -1,65 +1,22 @@
 package controllers;
 
-import inection.QuizDaoMock;
-import interfaces.Quiz;
-import interfaces.QuizDao;
-
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
- * Created by Mykolai_Lytvyn on 22.02.2016.
+ * Created by Mykolai_Lytvyn on 01.03.2016.
  */
-@WebServlet(name = "quiz.controller.QuizController", urlPatterns = {"/qquiz"})
+@WebServlet(name="quiz.controller.QuizAllController", urlPatterns = {"/quiz"})
 public class QuizController extends HttpServlet {
-    public static String PAGE_OK = "quiz.jsp";
-    public static String PAGE_ERROR = "error.html";
-    public static String ATTRIBUTE_QUIZ = "Quiz";
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
 
-
-    private QuizDao quizDao = new QuizDaoMock();
-    //private TransactionManager txManager = new TransactionManagerMock();
-
-    @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String idStr = "1";
-        System.out.println(idStr);
-        if (idStr != null && !idStr.equals("")) {
-            try {
-                final Integer id = Integer.valueOf(idStr);
-                Quiz quiz = selectById(id);
-
-                if (quiz != null) {
-                    System.out.println("quiz"+quiz);
-                    // this is map using when be transmitted data between server component
-                    request.setAttribute(ATTRIBUTE_QUIZ, quiz);
-                    //management capabilities container (in our case Tomcat) from component
-                    request.getRequestDispatcher(PAGE_OK).forward(request, response);
-                }
-            } catch (Exception ex) {
-                System.out.println("Server exception");
-            }
-        }else{
-            response.sendRedirect(PAGE_ERROR);
-        }
-        // ERROR
-
+        PrintWriter out = response.getWriter();
+        out.println("quiz");
     }
-
-    protected Quiz selectById(final Integer id) {
-        return quizDao.selectByID(id);
-
-       /* return txManager.doInTransaction(new Callable<Quiz>() {
-            @Override
-            public Quiz call() throws Exception {
-
-            }
-        });*/
-    }
-
-    ;
 }
